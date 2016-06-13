32100f1be55fbb2fb605a8fdfd56e6a2af69304e
Narendra Shetty <mailnarendra15@gmail.com>
2016-06-13 14:45:54 +0530

    generating initial state

--- src/scripts/game.js
+++ src/scripts/game.js
@@ -2,19 +2,26 @@ import Store from './store';
 
 const Game = function Game(size) {
   this.size = size;
+  this.$table = document.querySelector('.grid-container');
   this.setup();
 };
 
 Game.prototype.setup = function() {
   this.store = new Store();
-  this.store.subscribe(this.render);
+  this.store.subscribe(this.render.bind(this));
   this.store.dispatch({
     type: 'INIT'
   });
 };
 
-Game.prototype.render = function() {
-  console.log('rendering');
+Game.prototype.render = function(state) {
+  const grid = state.grid;
+  for(let i=0; i<grid.length; i++) {
+    let row = Math.floor(i / this.size);
+    let col = i % 4;
+    let node = this.$table.rows[row].cells[col];
+    node.innerHTML = grid[i];
+  }
 };
 
 export default Game;
--- src/scripts/store.js
+++ src/scripts/store.js
@@ -5,7 +5,7 @@ const initialState = {
 
 const Store = function Store() {
   this.oldState = {};
-  this.state = initialState;
+  this.state = {};
 };
 
 Store.prototype.subscribe = function(cb) {
@@ -14,11 +14,15 @@ Store.prototype.subscribe = function(cb) {
 
 Store.prototype.triggerSubscribers = function() {
   subscribers.forEach((subscriber) => {
-    subscriber();
+    subscriber(this.state);
   });
 };
 
 Store.prototype.reduce = function(action, state) {
+  switch (action.type) {
+    case 'INIT':
+      return this.getInitialState();
+  }
   return state;
 };
 
@@ -30,6 +34,17 @@ Store.prototype.dispatch = function(action) {
 
 Store.prototype.getState = function() {
   return this.state;
+};
+
+Store.prototype.getInitialState = function() {
+  let startCount = 2;
+  while (startCount > 0) {
+    let value = Math.random() < 0.9 ? 2 : 4;
+    let index = Math.floor(Math.random() * 16);
+    initialState.grid[index] = value;
+    startCount--;
+  }
+  return initialState;
 };
 
 
